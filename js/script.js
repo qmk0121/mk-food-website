875483516d17ac502959dc3bdcbd2d1b8ce8daf9
DYLAN MK <qmk0121@gmail.com>
2026-05-15 15:32:28 +0800

    Create JavaScript structure

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function toggleMenu(){
+    const menu = document.getElementById("nav-menu");
+    menu.classList.toggle("active");
+}
+
+function orderNow(){
+    alert("WhatsApp ordering feature will be added soon.");
+}
